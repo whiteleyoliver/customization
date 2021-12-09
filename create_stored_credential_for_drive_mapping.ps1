a344e125668f8fef064142ef647beeb1a0f33f77
whiteleyoliver <83054756+whiteleyoliver@users.noreply.github.com>
2021-12-09 14:27:09 -0600

    Add files via upload

---
+++ create_stored_credential_for_drive_mapping.ps1
@@ -0,0 +1,4 @@
+Install-Module -Scope AllUsers -Confirm:$false -Name credentialmanager
+$Credential = Get-Credential
+Remove-StoredCredential -Target files.whiteleyoliver.local -Type DomainPassword
+New-StoredCredential -Target files.whiteleyoliver.local -UserName $credential.username -Password $credential.password -Type DomainPassword -Persist Enterprise
