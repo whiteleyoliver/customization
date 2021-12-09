5bc6e42cbac7a085b63e9e833abe01a32ceb0e12
whiteleyoliver <83054756+whiteleyoliver@users.noreply.github.com>
2021-12-09 14:33:21 -0600

    Update create_stored_credential_for_drive_mapping.ps1

--- create_stored_credential_for_drive_mapping.ps1
+++ create_stored_credential_for_drive_mapping.ps1
@@ -1,4 +1,3 @@
-Install-Module -Scope AllUsers -Confirm:$false -Name credentialmanager
 $Credential = Get-Credential
 Remove-StoredCredential -Target files.whiteleyoliver.local -Type DomainPassword
 New-StoredCredential -Target files.whiteleyoliver.local -UserName $credential.username -Password $credential.password -Type DomainPassword -Persist Enterprise
