e554135ec0579d11d623feda616d24ee4ac91159
whiteleyoliver <83054756+whiteleyoliver@users.noreply.github.com>
2022-12-02 12:52:09 -0600

    Delete create_stored_credential_for_drive_mapping.ps1

--- create_stored_credential_for_drive_mapping.ps1
+++
@@ -1,3 +0,0 @@
-$Credential = Get-Credential
-Remove-StoredCredential -Target files.whiteleyoliver.local -Type DomainPassword
-New-StoredCredential -Target files.whiteleyoliver.local -UserName $credential.username -Password $credential.password -Type DomainPassword -Persist Enterprise
